e583dd433a2c006ee492fabd6e950b76edb4f5db
Adrian Wenzen <awenzen@hawk.iit.edu>
2026-06-03 15:18:08 +0700

    fix private key parsing

--- services/sheets.js
+++ services/sheets.js
@@ -12,7 +12,7 @@ async function getDoc() {
   if (!sheetsConfigured()) throw new Error('Google Sheets not configured');
   const serviceAccountAuth = new JWT({
     email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    key: (process.env.GOOGLE_PRIVATE_KEY || '').replace(/^"|"$/g, '').replace(/\\n/g, '\n'),
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   });
   const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID, serviceAccountAuth);
